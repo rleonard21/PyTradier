c911c18b7c40d982153668a181652f28b25badff
rleonard17 <bl.cirrus@gmail.com>
2017-07-02 00:00:02 -0400

    Added functionality to methods

--- pytradier/market/lookup.py
+++ pytradier/market/lookup.py
@@ -2,7 +2,6 @@ from ..base import Base
 from ..const import API_PATH
 
 class Lookup(Base):
-
     def __init__(self, **query):
         Base.__init__(self)
 
@@ -17,17 +16,10 @@ class Lookup(Base):
         if 'exchange' in query.keys():
             self._payload['exchanges'] = query['exchange']
 
-
-
-
-        print self._payload
-
         self._path = API_PATH['lookup']
         self._data = self._api_response(endpoint=self._endpoint,
                                         path=self._path,
                                         payload=self._payload)
-
-
 
     def _parse_response(self, attribute, **config):
         # returns the data from the API response in a dictionary for, {symbol0: data0, symbol1: data1, symbol2: data2}
@@ -44,31 +36,20 @@ class Lookup(Base):
         response_load = {}
 
         for response in self._data['securities']['security']:
-            print response
+            # more than one symbol in response, loop through each one
 
-            # more than one symbol supplied, loop through each one
-            if attribute in response.keys():
-                response_load[response['date']] = response[attribute]
-
-            else:
-                # this ensures that days when market is closed return None type if a market attribute is called.
-                response_load[response['date']] = None
+            response_load[response['symbol']] = response[attribute]
 
         return response_load
 
+    def symbol(self, **config):
+        return self._parse_response(attribute='symbol', **config)
 
+    def exchange(self, **config):
+        return self._parse_response(attribute='exchange', **config)
 
+    def type(self, **config):
+        return self._parse_response(attribute='type', **config)
 
-
-
-    def symbol(self):
-        pass
-
-    def exchange(self):
-        pass
-
-    def type(self):
-        pass
-
-    def desc(self):
-        pass
+    def desc(self, **config):
+        return self._parse_response(attribute='description', **config)
