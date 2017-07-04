d81f7a9050d969fe67ecac2df29bede8f3cf2dcc
rleonard17 <bl.cirrus@gmail.com>
2017-07-04 14:53:39 -0400

    Updated function name from super

--- pytradier/securities/option.py
+++ pytradier/securities/option.py
@@ -7,24 +7,24 @@ class Option(Quote):
         Quote.__init__(self, *symbols)  # init the Quote and Base classes as supers
 
     def strike(self, **config):
-        return self._api_quote(attribute='strike', **config)
+        return self._parse_response(attribute='strike', **config)
 
     def expiration(self, **config):
-        return self._api_quote(attribute='expiration_date', **config)
+        return self._parse_response(attribute='expiration_date', **config)
 
     def option_type(self, **config):
-        return self._api_quote(attribute='option_type', **config)
+        return self._parse_response(attribute='option_type', **config)
 
     def expiration_type(self, **config):
-        return self._api_quote(attribute='expiration_type', **config)
+        return self._parse_response(attribute='expiration_type', **config)
 
     def contract_size(self, **config):
-        return self._api_quote(attribute='contract_size', **config)
+        return self._parse_response(attribute='contract_size', **config)
 
     def underlying(self, **config):
-        return self._api_quote(attribute='underlying', **config)
+        return self._parse_response(attribute='underlying', **config)
 
     def open_interest(self, **config):
-        return self._api_quote(attribute='open_interest', **config)
+        return self._parse_response(attribute='open_interest', **config)
 
 
