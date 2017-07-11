1c55b59fce15387fe3dbeef9faeacf8abce01141
Robert Leonard <bl.cirrus@gmail.com>
2017-07-11 12:20:23 -0400

    added docs

--- pytradier/market/lookup.py
+++ pytradier/market/lookup.py
@@ -2,7 +2,20 @@ from ..base import Base
 from ..const import API_PATH
 
 class Lookup(Base):
+    """ A class for looking up symbols or partial symbols. The results are ordered by highest volume. 
+    The results are a dictionary with the symbol as the key and the information from each method as the value. 
+    """
     def __init__(self, **query):
+        """ Create an instance for the symbol lookup. 
+        
+        :param symbol: The requested search symbol. It can be a full or partial symbol. 
+        :param type: The type of symbol requested: ``stock``, ``etf``
+        :param exchange: The exchange of the symbol. As of current, only Tradier exchange symbols are accepted. 
+        
+        Any combination of these three parameters can be used to create a search. The results of the search are
+        stored in the instance of the class, which allows for the local storage of results rather than having to call to the API
+        for each piece of information. 
+        """
         Base.__init__(self)
 
         self._payload = {}
@@ -26,13 +39,43 @@ class Lookup(Base):
 
 
     def symbol(self, **config):
+        """ Return the symbol from the search. 
+        For example:
+        
+        .. code-block:: python
+        
+            tradier.market.lookup(symbol='AAPL').symbol()
+        """
         return self._parse_response(attribute='symbol', **config)
 
     def exchange(self, **config):
+        """ Return the exchange of the symbol. 
+        For example:
+        
+        .. code-block:: python
+        
+            tradier.market.lookup(symbol='AAPL').exchange()
+        """
         return self._parse_response(attribute='exchange', **config)
 
     def type(self, **config):
+        """ Return the type of symbol (``stock``, ``etf``, ``index``). 
+        For example:
+        
+        .. code-block:: python
+        
+            tradier.market.lookup(symbol='AAPL').type()
+        
+        """
         return self._parse_response(attribute='type', **config)
 
     def desc(self, **config):
+        """ Return a short description of the symbol. 
+        For example:
+        
+        .. code-block:: python
+        
+            tradier.market.lookup(symbol='AAPL').desc()
+        """
         return self._parse_response(attribute='description', **config)
+  
