83a7c017c3c406569166f1c1514ec58a73f20ac2
Robert Leonard <bl.cirrus@gmail.com>
2017-07-11 15:58:13 -0400

    added docs to search

--- pytradier/market/search.py
+++ pytradier/market/search.py
@@ -2,7 +2,7 @@ from ..base import Base
 from ..const import API_PATH
 
 class Search(Base):
-
+    """ A class for searching for a company. """
     def __init__(self, **query):
         Base.__init__(self)
 
@@ -23,15 +23,25 @@ class Search(Base):
         self._inner_key = 'symbol'
 
     def symbol(self, **config):
+        """ Return the symbol of the company. """
         return self._parse_response(attribute='symbol', **config)
 
     def exchange(self, **config):
+        """ Return the exchange where the symbol is located. 
+        
+        .. note::
+        
+            This method currently outputs Tradier's exchange codes.
+        
+        """
         return self._parse_response(attribute='exchange', **config)
 
     def type(self, **config):
+        """ Return the type of symbol (``etf``, ``stock``, ``index``). """
         return self._parse_response(attribute='type', **config)
 
     def desc(self, **config):
+        """ Return a short description of the company. """
         return self._parse_response(attribute='description', **config)
 
 
