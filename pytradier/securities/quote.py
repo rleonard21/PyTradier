7688553a49b6415711b9c7ce5d3c9519cbbb0a1b
Robert Leonard <bl.cirrus@gmail.com>
2017-07-11 11:10:14 -0400

    Added docs to quote

--- pytradier/securities/quote.py
+++ pytradier/securities/quote.py
@@ -48,6 +48,21 @@ class Quote(Base):
         return response_load
 
     def add_symbols(self, *symbols, **config):
+        """ Add one or more symbols to the array of tracked symbols. 
+        
+        :param *symbols: A string containing the company's symbol. 
+        
+        Multiple symbols can be added at once:
+        
+        .. code-block:: python
+        
+            stocks = tradier.Stock('AAPL', 'MSFT')  # create instance of the Stock class
+            print stocks.symbol()  # output: {AAPL: AAPL, MSFT: MSFT}
+            
+            stocks.add_sumbols('GOOG', 'TSLA')
+            print stocks.symbol()  # output: {AAPL: AAPL, MSFT: MSFT, GOOG: GOOG, TSLA: TSLA}
+            
+        """
         # adds a given symbol to the array of tracked symbols. the `update` parameter chooses whether or not to
         # call the API for new data
 
@@ -65,76 +80,101 @@ class Quote(Base):
             self.update_data()  # updates by default, user must specify to not update from the API
 
     def symbol(self, **config):
+        """ Return the symbol of each key in the dictionary. """
         return self._parse_response(attribute='symbol', **config)  # pass the desired variable in with the settings, if used
 
     def desc(self, **config):
+        """ Return a short description for each key. """
         return self._parse_response(attribute='description', **config)
 
     def exchange(self, **config):
+        """ Return the exchange on which the symbol is traded. """
+        # TODO: config option to return symbol name (NYSE, NASDAQ, etc.) or Tradier symbol (A, B, C, etc.)
         return self._parse_response(attribute='exch', **config)
 
     def type(self, **config):
         return self._parse_response(attribute='type', **config)
 
     def change(self, **config):
+        """ Return the dollar change. """
         return self._parse_response(attribute='change', **config)
 
     def change_percentage(self, **config):
+        """ Return the percentage change """
         return self._parse_response(attribute='change_percentage', **config)
 
     def volume(self, **config):
+        """ Return the volume for the day. """
         return self._parse_response(attribute='volume', **config)
 
     def average_volume(self, **config):
+        """ Return the average volume. """
         return self._parse_response(attribute='average_volume', **config)
 
     def last_volume(self, **config):
+        """ Return the latest volume. """
         return self._parse_response(attribute='last_volume', **config)
 
     def trade_date(self, **config):
+        """ Return the date and time of last trade in Unix Epoch time. """
         return self._parse_response(attribute='trade_date', **config)
 
     def open(self, **config):
+        """ Return the opening price. """
         return self._parse_response(attribute='open', **config)
 
     def high(self, **config):
+        """ Return the trading day high. """
         return self._parse_response(attribute='high', **config)
 
     def low(self, **config):
+        """ Return the trading day low. """
         return self._parse_response(attribute='low', **config)
 
     def close(self, **config):
+        """ Return the closing price. """
         return self._parse_response(attribute='close', **config)
 
     def prevclose(self, **config):
+        """ Return the previous closing price. """
         return self._parse_response(attribute='prevclose', **config)
 
     def week_52_high(self, **config):
+        """ Return the 52 week high. """
         return self._parse_response(attribute='week_52_high', **config)
 
     def week_52_low(self, **config):
+        """ Return the 52 week low. """
         return self._parse_response(attribute='week_52_low', **config)
 
     def bid(self, **config):
+        """ Return the latest bid price. """
         return self._parse_response(attribute='bid', **config)
 
     def bidsize(self, **config):
+        """ Return the size of the current bid. """
         return self._parse_response(attribute='bidsize', **config)
 
     def bidexch(self, **config):
+        """ Return the exchange of the current bid. """
         return self._parse_response(attribute='bidexch', **config)
 
     def bid_date(self, **config):
+        """ Return the date and time of the latest bid in Unix Epoch time. """
         return self._parse_response(attribute='bid_date', **config)
 
     def ask(self, **config):
+        """ Return the latest ask price. """
         return self._parse_response(attribute='ask', **config)
 
     def asksize(self, **config):
+        """ Return the size of the current ask. """
         return self._parse_response(attribute='asksize', **config)
 
     def askexch(self, **config):
+        """ Return the exchange of the current ask. """
         return self._parse_response(attribute='askexch', **config)
 
     def ask_date(self, **config):
+        """ Return the date and time of the latest ask in Unix Epoch time. """
         return self._parse_response(attribute='ask_date', **config)
