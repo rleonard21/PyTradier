5c74a95b1a0a3ef46a50e3a2b371cc700204d97b
Robert Leonard <bl.cirrus@gmail.com>
2017-07-11 11:13:00 -0400

    Added description for docs

--- pytradier/securities/stock.py
+++ pytradier/securities/stock.py
@@ -1,6 +1,6 @@
 from .quote import Quote
 
 class Stock(Quote):
-
+    """ A class for fetching and storing market data for stocks. """
     def __init__(self, *symbols):
         Quote.__init__(self, *symbols)  # init the Quote classes as supers
