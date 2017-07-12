726f0148cea442d94667ce75b195bb89fed5f619
Robert Leonard <bl.cirrus@gmail.com>
2017-07-12 14:55:57 -0400

    docs: added docstrings to tradier

--- pytradier/tradier.py
+++ pytradier/tradier.py
@@ -18,6 +18,33 @@ import os
 class Tradier:
 
     def __init__(self, token, account_id=None, endpoint=None):
+        """ Create an instance of the ``Tradier`` class. 
+        
+        :param token: The API access token provided by Tradier. `Required.`
+        :param account_id: The ID associated with your Tradier brokerage account. If not provided, you will only
+        be able to access market data. 
+        :param endpoint: The chosen endpoint. If not provided, it defaults to using the full API. For developer only
+        accounts, you must specify ``endpoint='sandbox'``.
+        
+        An instance of the ``Tradier`` class must be created in order to access any part of the Tradier API,
+        since the API is protected and the ``Tradier`` class contains your access token, account ID, and endpoint. 
+        
+        .. code-block:: python
+        
+            tradier = Tradier(token='a1b2c3d4e5', account_id='0123456789', endpoint=None)
+        
+        From here, all parts of the PyTradier library can be access through your instance of the ``Tradier`` class. 
+        For example, to retrieve the current market status:
+        
+        .. code-block:: python
+        
+            print tradier.market.status()  # output: open
+        
+        
+        
+        
+        
+        """
 
         os.environ["API_TOKEN"] = token  # create environment variable for all files to use
 
@@ -40,30 +67,22 @@ class Tradier:
 
         self.market = market.Market()
 
-
-
-
     def account(self):
+        """ Provide an instance of ``account``. """
         return account.Account()
 
     def company(self, symbol):
+        """ Provide an instance of ``company``. """
         return company.Company(symbol=symbol)
 
     def order(self):
+        """ Provide an instance of ``order``. """
         return order.Order()
 
     def stock(self, *symbols):
+        """ Provide an instance of ``stock``. This is the gateway to market data for stocks. """
         return stock.Stock(*symbols)
 
     def option(self, *symbols):
+        """ Provide an instance of ``option``. This is the gateway to market data for options. """
         return option.Option(*symbols)
-
-
-
-    # def user(self):
-    #     return user.User()
-
-
-
-
-    # curl -H "Authorization: Bearer  oUzA9JoInLiYTgTWA29l4CO3YVyJ" https://api.tradier.com/v1/markets/calendar
