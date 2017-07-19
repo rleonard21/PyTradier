4911f76e993a7278fec37c140727dcf8b35629d5
Robert Leonard <bl.cirrus@gmail.com>
2017-07-19 10:29:37 -0400

    Update history.py

--- pytradier/company/history.py
+++ pytradier/company/history.py
@@ -2,7 +2,21 @@ from ..base import Base
 from ..const import API_PATH
 
 class History(Base):
+    """ A class for handling historical pricing of companies. """
     def __init__(self, symbol, interval=None, start=None, end=None):
+        """ Create an instance of the History class. 
+        
+        :param symbol: The requested company symbol. By default, the parent ``Company`` class symbol is passed to this method. 
+        :param interval: Interval of time for the requested data set. Defaults to ``'daily'``, but can also take ``weekly`` and ``monthly``.
+        For example, if the interval is set to ``'monthly'``, then each piece of data represents one month. A smaller interval has more data. `Optional.` 
+        :param start: The start date of the data set in the format ``YYYY-MM-DD``. `Optional.`
+        :param end: The end date of the data set in the format ``YYYY-MM-DD``. `Optional.`
+        
+        .. note::
+        
+            Each method returns a dictionary with the date as the key and the requested data as the value. 
+        
+        """
         Base.__init__(self)
 
         self._symbol = symbol
@@ -31,18 +45,23 @@ class History(Base):
 
 
     def date(self):
+        """ Return the date in ISO format ``YYYY-MM-DD``. """
         return self._parse_response(attribute='date', update=False)
 
     def open(self):
+        """ Return the opening price for the interval. """
         return self._parse_response(attribute='open', update=False)
 
     def high(self):
+        """ Return the highest price from the interval. """
         return self._parse_response(attribute='high', update=False)
 
     def low(self):
+        """ Return the lowest point from the interval. """
         return self._parse_response(attribute='low', update=False)
 
     def volume(self):
+        """ Return the total volume for the interval. """
         return self._parse_response(attribute='volume', update=False)
 
 
