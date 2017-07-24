6ce0ecfa6d37d8a002ae71c8c37616718f2a0b52
Robert Leonard <bl.cirrus@gmail.com>
2017-07-24 10:33:22 -0400

    Update base.py

--- pytradier/base.py
+++ pytradier/base.py
@@ -78,3 +78,14 @@ class Base(object):
 
     def timestamp(self):
         return self.__last_updated
+    
+    def _data(**config):
+        """ Return the large, unorganized and unsorted data before PyTradier parses it. """
+        return self._parse_response(**config)
+        
+        
+        
+        
+        
+        
+        
