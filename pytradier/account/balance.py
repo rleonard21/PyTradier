b2c748cdfb35f7fe30ad118c1983ab93d4da03b1
rleonard17 <bl.cirrus@gmail.com>
2017-08-12 01:37:03 -0400

    Fixed dictionary keys for account balance

--- pytradier/account/balance.py
+++ pytradier/account/balance.py
@@ -23,111 +23,112 @@ class Balance(Base):
 
 
     def _parse_response(self, attribute, **config):
-        # returns the data from the API response in a dictionary for, {symbol0: data0, symbol1: data1, symbol2: data2}
-        # overrides from Base super since response must be a dictionary
-
         if 'update' in config.keys() and config['update'] is False:
-            # update the data if the `update` parameter is true
             pass
 
         else:
+            # update the data if the `update` parameter is true
             self.update_data()  # updates by default, user must specify to not update from the API
 
-        return self._data['balances'][attribute]
+        if 'inner' in config.keys():
 
+            return self._data['balances'][attribute][config['inner']]
+
+        else:
+            return self._data['balances'][attribute]
 
     def account_number(self, **config):
         return self._parse_response(attribute='account_number', **config)
 
     def account_type(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='account_type', **config)
 
     def cash_available(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='cash', inner='cash_available',**config)
 
     def close_pl(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='close_pl', **config)
 
     def current_requirement(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='current_requirement', **config)
 
     def day_trade_buying_power(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='day_trade_buying_power', **config)
 
     def dividend_balance(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='dividend_balance', **config)
 
     def equity(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='equity', **config)
 
     def fed_call(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='fed_call', **config)
 
     def long_liquid_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='long_liquid_value', **config)
 
     def long_market_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='long_market_value', **config)
 
     def maintenance_call(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='maintenance_call', **config)
 
     def market_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='market_value', **config)
 
     def net_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='net_value', **config)
 
     def open_pl(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='open_pl', **config)
 
     def option_buying_power(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='option_buying_power', **config)
 
     def option_long_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='option_long_value', **config)
 
     def option_requirement(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='option_requirement', **config)
 
     def option_short_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='option_short_value', **config)
 
     def pending_cash(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='pending_cash', **config)
 
     def pending_orders_count(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='pending_orders_count', **config)
 
     def sweep(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='cash', inner='sweep', **config)
 
     def short_liquid_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='short_liquid_value', **config)
 
     def short_market_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='short_market_value', **config)
 
     def stock_buying_power(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='stock_buying_power', **config)
 
     def stock_long_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='stock_long_value', **config)
 
     def stock_short_value(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='stock_short_value', **config)
 
     def uncleared_funds(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='uncleared_funds', **config)
 
     def unsettled_funds(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='cash', inner='unsettled_funds', **config)
 
     def total_cash(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='total_cash', **config)
 
     def total_equity(self, **config):
-        return self._parse_response(attribute='account_number', **config)
+        return self._parse_response(attribute='total_equity', **config)
 
 
 
