430d935f3101816f065c57c30132f86d71814177
rleonard21 <bl.cirrus@gmail.com>
2018-04-14 01:33:13 -0400

    Created closed position cost basis object

--- pytradier/account/costbasis.py
+++ pytradier/account/costbasis.py
@@ -1,2 +1,11 @@
-class CostBasis:
-    pass
+class ClosedPosition:
+    def __init__(self, close_date, open_date, term, cost, gl, gl_percent, proceeds, quantity, symbol):
+	    self.close_date = close_date
+	    self.open_date = open_date
+	    self.term = term
+	    self.cost = cost
+	    self.gain_loss = gl
+	    self.gain_loss_percent = gl_percent
+	    self.proceeds = proceeds
+	    self.quantity = quantity
+	    self.symbol = symbol
